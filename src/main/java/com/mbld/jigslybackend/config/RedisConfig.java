365b7e1ceec7c123389286d5a2eaece5439c4335
Bőti Marcell <de9d6m@inf.elte.hu>
2024-03-01 11:09:00 +0100

    init commit

--- src/main/java/com/mbld/jigslybackend/config/RedisConfig.java
+++ src/main/java/com/mbld/jigslybackend/config/RedisConfig.java
@@ -6,7 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -27,6 +31,7 @@ public class RedisConfig {
     JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         config.setDatabase(database);
+
         return new JedisConnectionFactory(config);
     }
 
